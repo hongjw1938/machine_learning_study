ccff6bbbc691c312cd08353c4ae8053c7ab4db7b
Hong JW <sighingtree@naver.com>
2018-05-14 01:33:21 +0000

    cross entropy error

--- python_class/deep_learning/deep learning from scratch/neural_network_04.py
+++ python_class/deep_learning/deep learning from scratch/neural_network_04.py
@@ -12,3 +12,34 @@ print(mean_squared_error(np.array(y), np.array(t)))
 y = [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0]
 
 print(mean_squared_error(np.array(y), np.array(t)))
+
+
+##교차 엔트로피 오차
+
+import numpy as np
+
+def mean_squared_error(y, t):
+    '''
+    평균 제곱 오차를 반환합니다.
+    
+    입력:
+       y: 출력 데이터
+       x: 타겟 레이블, 원-핫 인코딩
+    '''
+    return 0.5 * np.sum((y - t) ** 2)
+    
+    
+def cross_entropy_error(y, t):
+    delta = 1e-7
+    return -np.sum(t * np.log(y + delta))
+    
+
+cross_entropy_error(y, t)
+
+y = np.array([0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0])
+
+cross_entropy_error(y, t)
+
+
+t = np.array([0, 0, 1, 0, 0, 0, 0, 0, 0, 0])
+y = np.array([0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0])
